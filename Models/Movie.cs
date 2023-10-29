0711853b5df0feb83c757bc6143a7e9f3ac759c7
Nathan Krieger <nkrieger0444@gmail.com>
2023-10-29 11:07:47 -0700

    Add display name attribute to Movie model

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie.Models;
@@ -7,6 +8,7 @@ public class Movie
     public int Id { get; set; }
     public string? Title { get; set; }
     [DataType(DataType.Date)]
+    [DisplayName("Release Date")]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
     public decimal Price { get; set; }
